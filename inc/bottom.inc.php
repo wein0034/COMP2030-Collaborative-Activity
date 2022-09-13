71630fb4655947078143a257018e346218eadbd4
rich0485 <rich0485@flinders.edu.au>
2022-09-13 11:37:56 +0930

    djalkfs

--- inc/bottom.inc.php
+++ inc/bottom.inc.php
@@ -1,6 +1,7 @@
 <nav id="bottombar">
-    <a href="">About</a>
-    <a href="">Help and Support</a>
-    <a href="">Contact Us</a>
-    <a href="">Site Map</a>
+    <a href="">About |</a>
+    <a href="">Help and Support |</a>
+    <a href="">Contact Us |</a>
+    <a href="">Site Map |</a>
+    © 2022, SENIOR, Inc.
 </nav>
