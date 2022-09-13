4bae686999bfb625f188d3d4e96a3980aa454058
rich0485 <rich0485@flinders.edu.au>
2022-09-13 10:35:45 +0930

    testing bottom bar

---
+++ bottom.php
@@ -0,0 +1,6 @@
+<ul id="bottombar">
+    <li><a href="">About</a></li>
+    <li><a href="">Help and Support</a></li>
+    <li><a href="">Contact uUs</a></li>
+    <li><a href="">Site Map</a></li>
+</ul>
