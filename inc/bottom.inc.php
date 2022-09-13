02fe3aa41ef3bdd2a433057f3fe385f1c39dea32
rich0485 <rich0485@flinders.edu.au>
2022-09-13 11:26:09 +0930

    bottombar changes

--- inc/bottom.inc.php
+++ inc/bottom.inc.php
@@ -1,6 +1,6 @@
-<ul id="bottombar">
-    <li><a href="">About</a></li>
-    <li><a href="">Help and Support</a></li>
-    <li><a href="">Contact Us</a></li>
-    <li><a href="">Site Map</a></li>
-</ul>
+<nav id="bottombar">
+    <a href="">About</a>
+    <a href="">Help and Support</a>
+    <a href="">Contact Us</a>
+    <a href="">Site Map</a>
+</nav>
