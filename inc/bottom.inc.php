9a149ccc6b7a01c503ef4ffa06ed4a00c19a92aa
rich0485 <rich0485@flinders.edu.au>
2022-09-18 11:59:34 +0930

    bottom bar adjustments

--- inc/bottom.inc.php
+++ inc/bottom.inc.php
@@ -1,7 +1,7 @@
-<nav id="bottombar">
-    <a href="">About |</a>
-    <a href="">Help and Support |</a>
-    <a href="">Contact Us |</a>
-    <a href="">Site Map |</a>
-    © 2022, SENIOR, Inc.
-</nav>
+<li class="bottombar">
+    <ul><a href="">About</a></ul>
+    <ul><a href="">Help and Support</a></ul>
+    <ul><a href="">Contact Us</a></ul>
+    <ul><a href="">Site Map</a></ul>
+    <ul class ="copyright">© 2022, SENIOR, Inc.</ul>
+</li>
