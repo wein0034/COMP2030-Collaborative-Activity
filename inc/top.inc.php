f095fd0038fb9a66df8fd686a492cfa671937675
tatianaroa <tatianarja@gmail.com>
2022-09-20 22:59:01 +0930

    search bar position

--- inc/top.inc.php
+++ inc/top.inc.php
@@ -2,7 +2,8 @@
     <a href="./account.php">Account</a> 
     <a href="">Sign up</a>
     <a href="./login.php">Log in</a>
+    <?php require_once "inc/search.inc.php"; ?>
     <a href="">Buy</a>
     <a href="./listitem.php">Sell</a>
-    <?php require_once "inc/search.inc.php"; ?>
+    
 </nav>
