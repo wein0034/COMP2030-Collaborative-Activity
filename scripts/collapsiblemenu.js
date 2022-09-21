aa3ea2675ee858693c3c91936dbcf037864bd63a
wein0034 <wein0034@flinders.edu.au>
2022-09-21 18:21:00 +0930

    Collapsibles can now be forced to only allow one item to be expanded at once. Logic for collapsibles moved to collapsiblemenu.js. login.js only contains code for login.php now. Added comments to collapsiblemenu.js. .innerlogin in style.css modifed to have consistent height between sign up and log in. This is a temorary thing and should be refined later.

--- scripts/collapsiblemenu.js
+++ scripts/collapsiblemenu.js
@@ -1,18 +1,71 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+var allowMultipleExpanded = new Boolean(true);
+var collapsibles;
 
-// temporarily using login.js for the login page collapsibles
-// will modify this script to modularly work across all collapsible pages
-// ~Julian
+Init()
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
+function Init()
+{
+  // add an event listener to every element with the "collapsible" class
+  collapsibles = document.getElementsByClassName("collapsible");
+  for (var i = 0; i < collapsibles.length; i++) 
+  {
+    collapsibles[i].addEventListener("click", function () { ToggleCollapse(event.target) });  // ToggleCollapse loaded from collapsiblemenu.js
+  }
+}
+
+function ToggleCollapse(elem) 
+{
+  // toggle whether the element passed as a parameter (elem) is currently collapsed
+  elem.classList.toggle("active");
+  var content = elem.nextElementSibling;
+  if (content.style.display === "block")
+  {
+    content.style.display = "none";
+  }
+  else
+  {
+    content.style.display = "block";
+  }
+
+  // if only one element should be expanded at once, iterate through every other element and set it to be collapsed
+  if (!allowMultipleExpanded)
+  {
+    for (var i = 0; i < collapsibles.length; i++) 
+    {
+      if (collapsibles[i] != elem)
+      {
+        SetCollapse(collapsibles[i], true);
+      }
     }
-  });
-} 
+  }
+}
+
+// set certain elements to be collapsed or not
+function SetCollapse(elem, collapse)
+{
+  var content = elem.nextElementSibling;
+
+  if (collapse)
+  {
+    elem.classList.remove("active");
+    content.style.display = "none";
+  }
+  else
+  {
+    elem.classList.add("active");
+    content.style.display = "block";
+  }
+}
+
+// allow external scripts (eg login.js) to customise this setting
+function SetAllowMultipleExpanded(set)
+{
+  if (set === true)
+  {
+    allowMultipleExpanded = true;
+  }
+  else
+  {
+    allowMultipleExpanded = false;
+  }
+}
--- scripts/login.js
+++ scripts/login.js
@@ -1,41 +1,18 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
 Init();
 
 function Init() 
 {
-  for (i = 0; i < coll.length; i++) 
-  {
-    coll[i].addEventListener("click", function () { ToggleCollapse(event.target) });
-  }
+  SetAllowMultipleExpanded(false);  //SetAllowMultipleExpanded loaded from collapsiblemenu.js
 
-  if (getUrlParam("returning") === "true")
+  if (getUrlParam("returning") === "true")  // getUrlParam loaded from helperfunctions.js
   {
     
     console.log("Entered via Log In");
-    ToggleCollapse(coll[1]);
+    ToggleCollapse(collapsibles[1]);
   }
   else
   {
     console.log("Entered via Sign Up");
-    ToggleCollapse(coll[0]);
+    ToggleCollapse(collapsibles[0]);
   }
 }
-
-function ToggleCollapse(elem) 
-{
-  elem.classList.toggle("active");
-  var content = elem.nextElementSibling;
-
-  if (content.style.display === "block")
-  {
-    content.style.display = "none";
-  }
-  else
-  {
-    content.style.display = "block";
-  }
-}
-
-
