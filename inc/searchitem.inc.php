1117eaf914147162187eb063cc059309f5ca5c45
wein0034 <wein0034@flinders.edu.au>
2022-09-21 22:54:02 +0930

    Search result expanded content basics added.

--- inc/searchitem.inc.php
+++ inc/searchitem.inc.php
@@ -19,12 +19,10 @@
                         <h3>Seller Name</h3>
                     </div>
                 </div>
-                
-                
             </div>
             
             <div id="srRating">
-                <h1>★★★★★</h1>
+                <h1>★ ★ ★ ★ ★</h1>
             </div>
             
             <div id="srPrice">
@@ -36,7 +34,38 @@
         </div>
     </div>
 
-    <div class="searchresult">
-        <p>content</p>
+    <div class="collapsableContent">
+        <div class="searchresult">
+            <div id="contentImage">
+                <img id="tent" src="images/tent.jpg" alt="Item Preview">
+            </div>
+
+            <div id="contentDetails">
+                <div id="contentText">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui.</p>
+                </div>
+                <div id="contentReviews">
+                    <div id="itemReviews">
+                        <h3>Item</h3>
+                        <h1>★ ★ ★ ★ ★</h1>
+                        <h3>100 Reviews</h3>
+                    </div>
+                    <div id="sellerReviews">
+                        <h3>Seller</h3>
+                        <h1>★ ★ ★ ★ ★</h1>
+                        <h3>50 Reviews</h3>
+                    </div>
+                </div>
+            </div>
+
+            <div id="contentButtons">
+                <button type="submit" class="contentButton">Add to Cart</button>
+                <button type="submit" class="contentButton">View more info</button>
+            </div>
+            
+        </div>
     </div>
 </div>
+
+
+
