76344c98db2600140e77205573907f65d6ecc77f
rich0485 <rich0485@flinders.edu.au>
2022-09-24 14:49:09 +0930

    f

--- viewitem.php
+++ viewitem.php
@@ -13,9 +13,28 @@
     <?php require_once "inc/search.inc.php"; ?>
 
 
-    
+    <div class="item">
+        <div class="column">
+                <img id="tent" src="images/tent.jpg" alt="Item Preview" width=600px><br>
+                <img src="images/addphoto.png" alt="Item Preview" width=150px>
+                <img src="images/addphoto.png" alt="Item Preview" width=150px>
+                <img src="images/addphoto.png" alt="Item Preview" width=150px>
+                <img src="images/addphoto.png" alt="Item Preview" width=150px>
+        </div>
 
+        <div class="column">
 
+                    <h1>Item Name</h1>
+                    <h1>$00.00</h1>
+                <hr>
+                        <h3>Category</h3> 
+                        <h3>Seller Name</h3>
+
+                
+        </div>
+  
+      
+    </div>
 
     <?php require_once "inc/bottom.inc.php"; ?>
 </body>
