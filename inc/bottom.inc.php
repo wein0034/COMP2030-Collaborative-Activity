6807b21f71c93084784e1c9681083741b34ab2f1
rich0485 <rich0485@flinders.edu.au>
2022-09-24 15:37:44 +0930

    fixed footer (sort of)

--- inc/bottom.inc.php
+++ inc/bottom.inc.php
@@ -1,7 +1,11 @@
-<li class="bottombar">
-    <ul><a href="">About</a></ul>
-    <ul><a href="">Help and Support</a></ul>
-    <ul><a href="">Contact Us</a></ul>
-    <ul><a href="">Site Map</a></ul>
-    <ul class ="copyright">© 2022, SENIOR, Inc.</ul>
-</li>
+<nav class="bottombar">
+    <div id="leftfooter">
+    <a href="./about.php">About</a></ul>
+    <a href="./help.php">Help and Support</a></ul>
+    <a href="./contactus.php">Contact Us</a></ul>
+    <a href="./sitemap.php">Site Map</a></ul>
+</div>
+<div id="rightfooter">
+    © 2022, SENIOR, Inc.
+</div>
+</nav>
