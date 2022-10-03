c4be86c77ff8394eb88bb6e9898e32636a4ff86b
wein0034 <wein0034@flinders.edu.au>
2022-10-03 20:30:46 +1030

    Very minor formatting changes.

--- account.php
+++ account.php
@@ -15,41 +15,39 @@
     <div class="account">
         <div id="content-wrap">
         <p><h1>Account</h1><p>
-        <div class="row1">
-            <div class="leftHalf">
-                <h2> Your Ads </h2>
-                <h3 id='productName'>Caravan</h3>
-                <p id='productPrice'>$30,000<p>
-                <p id='productDescription'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. 
-                    Duis vulputate commodo lectus, ac blandit elit tincidunt id.</p>
-                <img src="images/caravan.jpg" class="" width=100%>
-
+            <div class="row1">
+                <div class="leftHalf">
+                    <h2> Your Ads </h2>
+                    <h3 id='productName'>Caravan</h3>
+                    <p id='productPrice'>$30,000<p>
+                    <p id='productDescription'> 
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. 
+                        Duis vulputate commodo lectus, ac blandit elit tincidunt id.
+                    </p>
+                    <img src="images/caravan.jpg" class="" width=100%>
+                </div>
+                
+                <div class="rightHalf">
+                    <h2> Your Orders </h2>
+                    <h3 id='productName'>Tent<h3>
+                    <img src="images/tent.jpg" class="" width=100%>
+                </div>
             </div>
-            
-            <div class="rightHalf">
-                <h2> Your Orders </h2>
-                <h3 id='productName'>Tent<h3>
-                <img src="images/tent.jpg" class="" width=100%>
-
+                
+            <div class="row2">
+                <div class="leftHalfR2">
+                    <h2 id='row2Tittle'> Favourites </h2>
+                    <img src="images/favourite.png" class="favouriteIcon">
+                </div>
+                
+                <div class="rightHalfR2">
+                    <h2 id='row2Tittle'> Settings </h2>
+                    <img src="images/setting.png" class="settingIcon">
+                </div>
             </div>
-
-        </div>
-            
-        <div class="row2">
-            <div class="leftHalfR2">
-                <h2 id='row2Tittle'> Favourites </h2>
-                <img src="images/favourite.png" class="favouriteIcon">
-            </div>
-            
-            <div class="rightHalfR2">
-                <h2 id='row2Tittle'> Settings </h2>
-                <img src="images/setting.png" class="settingIcon">
-            </div>
-
         </div>
     </div>
-</div>
 
     <?php require_once "inc/bottom.inc.php"; ?>
 </body>
--- inc/bottom.inc.php
+++ inc/bottom.inc.php
@@ -1,11 +1,12 @@
 <nav class="bottombar">
     <div id="leftfooter">
-    <a href="./about.php">About</a></ul>
-    <a href="./help.php">Help and Support</a></ul>
-    <a href="./contactus.php">Contact Us</a></ul>
-    <a href="./sitemap.php">Site Map</a></ul>
-</div>
-<div id="rightfooter">
-    &copy; 2022, SENIOR, Inc.
-</div>
+        <a href="./about.php">About</a>
+        <a href="./help.php">Help and Support</a>
+        <a href="./contactus.php">Contact Us</a>
+        <a href="./sitemap.php">Site Map</a>
+    </div>
+
+    <div id="rightfooter">
+        &copy; 2022, SENIOR, Inc.
+    </div>
 </nav>
