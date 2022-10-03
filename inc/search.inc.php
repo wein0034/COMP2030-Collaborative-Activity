28c2657fd4194f0a54009eae541b3306d9735500
rich0485 <rich0485@flinders.edu.au>
2022-10-04 10:25:22 +1030

    added a logo

--- inc/search.inc.php
+++ inc/search.inc.php
@@ -1,6 +1,8 @@
 <script src="scripts/helperfunctions.js" defer></script>
 <script src="scripts/searchbar.js" defer></script>
 
+<img src="images/logo.png" id="logo">
+
 <form action="searchresults.php" class="searchBar" method="GET">
     <input type="text" id="searchQuery" name="searchQuery" placeholder="I'm looking for..." class="searchBarText">
     <input type="submit" value="Search" class="searchBarButton">
