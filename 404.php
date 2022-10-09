e6b82196d48de7bb9a45b6e3c6fc1ad6e90a1b03
rich0485 <rich0485@flinders.edu.au>
2022-10-09 12:11:14 +1030

    404 page

---
+++ 404.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>SENIOR - New and Returning Users</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="rich0485, roaj0001, wein0034">
+    
+    <script src="scripts/helperfunctions.js" defer></script>
+    <script src="scripts/collapsiblemenu.js" defer></script>
+    <script src="scripts/login.js" defer></script>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+
+    <?php require_once "inc/top.inc.php"; ?> 
+    <?php require_once "inc/search.inc.php"; ?>
+    <?php require_once "inc/register.inc.php"; ?> 
+
+<body>
+
+<div class="construction-container">
+<div class="construction-bg"></div>
+    <div class="construction-content">
+            <h1>404...</h1><p>
+            <h2>Page under construction</h2><p>
+            <button onclick="history.back()" id="back">Go Back</button>
+    </div>    
+</div>
+
+
+
+    <?php require_once "inc/bottom.inc.php"; ?>
+</body>
+</html>
