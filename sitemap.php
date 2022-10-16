142f17d81f9beab7c0486c1250c39f1d7abba2f2
rich0485 <rich0485@flinders.edu.au>
2022-10-16 13:18:08 +1030

    fixed sitemap

---
+++ sitemap.php
@@ -0,0 +1,28 @@
+<!-- contributor: Jessica Richardson rich0485 -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>SENIOR -- Site Map</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="rich0485, roaj0001, wein0034">
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+
+<body class="listitem-background">
+    <?php require_once "inc/top.inc.php"; ?>
+    <?php require_once "inc/sell.inc.php"; ?>
+    <?php require_once "inc/search.inc.php"; ?>
+
+    <div class="sitemap">
+        <div class="sitemap-c">
+            <h1>Site Map</h1>
+            <p><img src="images/sitemap.png"></p>
+        </div>
+    </div>
+
+    <?php require_once "inc/bottom.inc.php"; ?>
+
+</body>
+</html>
